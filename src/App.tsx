e66be3667e81eb1273f9719a0cb7c79d4713695d
André <andredev.fe@gmail.com>
2025-09-24 22:48:03 -0300

    fix: refactor App component structure for improved readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,26 @@
-import AboutSection from "./components/about";
-import { ContactSection } from "./components/contact-section";
-import HeroSection from "./components/Hero";
-import Navbar from "./components/navbar";
-import { ProjectsSection } from "./components/projects-section";
-import Technologies from "./components/Technologies";
-import { Toaster } from "./components/ui/toaster";
+import AboutSection from './components/about';
+import { ContactSection } from './components/contact-section';
+import HeroSection from './components/Hero';
+import Navbar from './components/navbar';
+import { ProjectsSection } from './components/projects-section';
+import Technologies from './components/Technologies';
+import { Toaster } from './components/ui/toaster';
 
 function App() {
   return (
-    <section id="home">
-      <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
-        <div className="fixed top-0 -z-10 h-full w-full">
-          <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-        </div>
-        <div className="container mx-auto px-0">
-          <Navbar />
-          <HeroSection />
-          <AboutSection />
-          <Technologies/>
-          <ProjectsSection />
-          <ContactSection />
-          <Toaster />
-        </div>
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+      <div className="fixed inset-0 -z-10 bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <div className="container relative z-0 mx-auto px-0">
+        {' '}
+        <Navbar />
+        <HeroSection />
+        <AboutSection />
+        <Technologies />
+        <ProjectsSection />
+        <ContactSection />
+        <Toaster />
       </div>
-    </section>
+    </div>
   );
 }
 
